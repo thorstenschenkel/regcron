e5c82468037ea5cc519d00a1cba3d3e1c8771db3
COMPEOPLE\tsc <thorsten.schenkel@compeople.de>
2018-04-26 10:38:26 +0200

    first try to use update

--- src/dbHelper.js
+++ src/dbHelper.js
@@ -1,6 +1,10 @@
 const MongoClient = require('mongodb').MongoClient;
 const EventDb = require('./model/eventDb');
 
+// https://github.com/mongodb/node-mongodb-native
+// http://mongodb.github.io/node-mongodb-native/3.0/reference/ecmascriptnext/crud/
+// http://mongodb.github.io/node-mongodb-native/3.0/tutorials/crud/#crud-operations
+
 const DB_NAME = 'reg';
 const DB_COLLECTION = 'snapshots';
 
@@ -23,16 +27,36 @@ class DbHelper {
     async storeEventInDB(event) {
 
         const eventDb = this._createEventDb(event);
+        const msg = `-- t7 - DBG -- store event: ${event.name})`;
+        console.log(msg);
+
+        // try {
+        //     const client = await MongoClient.connect(this.dbUri);
+        //     console.log("Connected correctly to server");
+        //     const db = client.db(DB_NAME);
+        //     const insertRet = await db.collection(DB_COLLECTION).insertOne(eventDb);
+        //     if (!insertRet || insertRet.insertedCount != 1) {
+        //         console.error(' -- t7 -- ERR -- can not insert event!');
+        //     }
+        //     client.close();
+        // } catch (err) {
+        //     console.error(' -- t7 -- ERR -- store error: ', err.stack);
+        // }
 
         try {
             const client = await MongoClient.connect(this.dbUri);
             console.log("Connected correctly to server");
             const db = client.db(DB_NAME);
-            const insertRet = await db.collection(DB_COLLECTION).insertOne(eventDb);
-            if (!insertRet || insertRet.insertedCount != 1) {
-                console.error(' -- t7 -- ERR -- can not insert event!');
+            const query = { name: eventDb.name };
+            const options = { upsert: true };
+            const updateResult = await db.collection(DB_COLLECTION).updateOne(query, eventDb, options);
+            if (!updateResult || (updateResult.nUpserted === 0 && updateResult.nModified === 0)) {
+                console.error(' -- t7 -- ERR -- can not store/update event!');
+            } else {
+                const msg = `-- t7 - DBG -- stored : ${updateResult.nUpserted} / ${updateResult.nModified})`;
+                console.log(msg);
             }
-            client.close();
+                client.close();
         } catch (err) {
             console.error(' -- t7 -- ERR -- store error: ', err.stack);
         }
